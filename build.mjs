c96c7b35ddce7795367a1e44f0defbbc0d40c240
Derk-Jan Karrenbeld <derk-jan@ns.com>
2026-01-25 06:49:29 +0800

    Automatically export output to sample

--- build.mjs
+++ build.mjs
@@ -1,5 +1,5 @@
-import { build } from "esbuild";
 import browserslistToEsbuild from "browserslist-to-esbuild";
+import { build } from "esbuild";
 import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
 import { mockFsPlugin } from "./build/mock-fs.mjs";
 import { join } from "node:path";
@@ -9,9 +9,9 @@ function toBoolean(value) {
   return value === "1" || value === "true" || value === 1 || value === true;
 }
 
-import { copyFileSync, glob, globSync } from "node:fs"
-import { basename, join } from "node:path"
 import { spawnSync } from "node:child_process";
+import { copyFileSync } from "node:fs";
+import { basename, join } from "node:path";
 
 
 await Promise.all([
